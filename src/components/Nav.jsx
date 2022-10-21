eaae865325dd4737860b8ce746a8f5a4b43f7fb9
Dildeep <dildeepgill@hotmail.com>
2022-10-21 18:24:36 -0400

    git init testing

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Nav() {
+  return (
+    <nav>
+      <p>LOGO</p>
+      <a href="/">Home</a>
+      <a href="games">Games</a>
+      <FontAwesomeIcon icon="shopping-cart" />
+    </nav>
+  );
+}
+export default Nav;
