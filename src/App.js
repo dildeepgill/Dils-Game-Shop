a112b241e2e2ed3f4e2675b38f85d0f117ab31e9
Dildeep <dildeepgill@hotmail.com>
2022-10-23 16:08:16 -0400

    added cart updating function

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import Home from './Pages/Home';
 import {Route, Routes} from "react-router-dom"
 import Allgames from './Pages/Allgames';
 import More from './Pages/More';
+import Cart from './Pages/Cart';
 
 
 
 function App() {
   const [games, getGames]=useState(null)
+  const [cart, setCart]= useState([])
+  
+  function add(singleGames){
+    setCart( (prevState) =>{
+
+    })
+  }
   
   useEffect(() =>{
-
 axios
   .get("https://api.rawg.io/api/games?key=57d57c4f17ab4eb6be853b6c81c89343")
   .then((res)=>{
@@ -31,8 +38,8 @@ axios
       <Routes>             
           <Route path="/" exact element= {games&&<Home games={games} />}/>
           <Route path="/Allgames"  element={games&&<Allgames games={games}/>}/>
-          <Route path="/games/:id"  element={games&&<More games={games}/>}/>
-
+          <Route path="/games/:id"  element={games&&<More games={games} add={add}/>}/>
+          <Route path="/games/cart"  element={games&&<Cart games={games} cart={cart}/> }/>
      </Routes>  
       <Footer/>   
 </>
